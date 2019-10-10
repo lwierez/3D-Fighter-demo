f20b20a7ce8851ff28f210ebe2a15fb07e165a08
lwierez <loic.wierez@viacesi.fr>
2019-10-10 15:09:34 +0200

    Added player
    Added the player model
    Added funtions to control the plane
    Added basic camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	protected Vector3 Offset;
+
+    void Start()
+    {
+		Offset = new Vector3(0.0f, 1.6f, -3.32f);
+    }
+
+    void Update()
+    {
+		transform.position = player.transform.position + Offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 20.0f;
+	public float rotationSpeedVertical = 45.0f;
+	public float rotationSpeedHorizontal = 90.0f;
+
+	protected float horizontalInput;
+	protected float verticalInput;
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		transform.Rotate(Vector3.right, rotationSpeedVertical * Time.deltaTime * verticalInput);
+		transform.Rotate(Vector3.forward, rotationSpeedHorizontal * Time.deltaTime * horizontalInput);
+    }
+}
