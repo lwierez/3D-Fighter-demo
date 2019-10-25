905a6ae8bc09a42b3545f6dfcb6213c4fc4c0a20
Loic WIEREZ <loic.wierez@viacesi.fr>
2019-10-25 12:14:51 +0200

    Replaced test cubes by enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMove : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+	public Transform playerTransform;
+
     void FixedUpdate()
     {
+		transform.forward = (playerTransform.position - transform.position).normalized;
 		transform.Translate(Vector3.forward * Time.deltaTime * 15);
     }
 }
